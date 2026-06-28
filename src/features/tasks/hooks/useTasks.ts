8db03f5adabcb625e3e124aba3425b21fc1fafc6
Ruslan Dovhan <dovganr165@gmail.com>
2026-06-28 13:04:51 +0300

    local storage

--- src/features/tasks/hooks/useTasks.ts
+++ src/features/tasks/hooks/useTasks.ts
@@ -6,18 +6,29 @@ import { fetchTasks } from '../services/taskService';
 export const useTasks = () => {
     const [tasks, setTasks] = useState<Task[]>([]);
     const [loading, setLoading] = useState<boolean>(true);
+    const [isLoaded, setIsLoaded] = useState(false);
 
 
     useEffect(() => {
-        const loadTasks = async () => {
-            setLoading(true);
-            const data = await fetchTasks();
-            setTasks(data);
+        const load = async () => {
+            const saved = localStorage.getItem("tasks");
+            if (saved) {
+                setTasks(JSON.parse(saved));
+            } else {
+                const data = await fetchTasks();
+                setTasks(data);
+            }
+            setIsLoaded(true);
             setLoading(false);
         };
-
-        loadTasks();
+        load();
     }, []);
+
+    useEffect(() => {
+        if (isLoaded) {
+            localStorage.setItem("tasks", JSON.stringify(tasks));
+        }
+    }, [tasks, isLoaded]);
 
     function addTask(title: string) {
         const newTask: Task = {
